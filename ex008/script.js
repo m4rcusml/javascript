44d28aaf0708915ec8239d17cec9d7915b74ec0d
Marcus com U <114195461+m4rcusml@users.noreply.github.com>
2023-01-21 23:39:55 -0400

    ex008 feito

---
+++ ex008/script.js
@@ -0,0 +1,49 @@
+class Item {
+    constructor(valor) {
+        this.valor = Number(valor)
+    }
+
+    isNasRegras() {
+        if (this.valor >= 0 && this.valor <= 100) {
+            return true
+        }
+        return false
+    }
+
+    alreadyExist(vetor) {
+        if(vetor.indexOf(this.valor) != -1) {
+            return true
+        }
+        return false
+    }
+
+    criarItem() {
+        let option = document.createElement('option')
+        option.text = `${this.valor} foi adicionado`
+        return option
+    }
+}
+
+document.addEventListener('keypress', function (evt) {
+    if (evt.key == 'Enter') {
+        var btnAdicionar = document.querySelector('#btnAdicionar')
+        btnAdicionar.click()
+    }
+})
+
+var itens = []
+var select = document.querySelector('#select')
+
+function adicionar() {
+    let numero = document.querySelector('#numero')
+    let item = new Item(numero.value)
+
+    if (item.isNasRegras() && !item.alreadyExist(itens)) {
+        let option = item.criarItem()
+        itens.push(item.valor)
+        select.appendChild(option)
+        select.size = `${itens.length}`
+    } else {
+        alert('O número inserido não está no intervalo sugerido ou já existe.')
+    }
+}
